0eb565c2f62beb2a1aa07c4471dc14b9ae94f7a0
Akshat <aks_shukla@outlook.com>
2026-02-15 14:01:10 +0530

    Initial Commit

---
+++ Foldy/FoldyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoldyApp.swift
+//  Foldy
+//
+//  Created by Akshat Shukla on 15/02/26.
+//
+
+import SwiftUI
+
+@main
+struct FoldyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
